373aef993f35609f9cd824257a1c0a738b2c738e
jcuello123 <jcuello123@yahoo.com>
2022-07-15 21:15:04 -0400

    say hello

--- src/main/java/com/cuello/jurnal/api/logs/LogsEndpoint.java
+++ src/main/java/com/cuello/jurnal/api/logs/LogsEndpoint.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LogsEndpoint {
     @GetMapping
     public ResponseEntity index() {
-        return new ResponseEntity("hi", HttpStatus.OK);
+        return new ResponseEntity("hello", HttpStatus.OK);
     }
 }
